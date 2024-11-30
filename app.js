4cfa6e14d161418cd2271791985a7bd481ad78db
saagor16 <saagor.16@gmail.com>
2024-11-30 19:35:51 +0600

    cors use

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 import express from "express";
-import { config } from "dotenv";
 import cors from "cors";
 
 const app = express();
 const router = express.Router();
 
+// Middleware
+app.use(express.json());
+app.use(cors());
 
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["POST"],
+      credentials: true,
+    })
+  );
+  
+
+// Define a test route
+router.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+app.use("/", router);
+
+// Set PORT directly in the code
+const PORT = 3000; // Replace 3000 with your desired port
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
