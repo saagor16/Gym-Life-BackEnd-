16773b9aceee6c33726f93e5921d63f460238978
saagor16 <saagor.16@gmail.com>
2024-11-30 19:44:13 +0600

    email rout add

--- app.js
+++ app.js
@@ -9,19 +9,47 @@ app.use(express.json());
 app.use(cors());
 
 app.use(
-    cors({
-      origin: ['http://localhost:5173/'],
-      methods: ["POST"],
-      credentials: true,
-    })
-  );
-  
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["POST"],
+    credentials: true,
+  })
+);
 
-// Define a test route
-router.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-app.use("/", router);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+router.post("/send/mail", async (req, res, next) => {
+    const { name, email, message } = req.body;
+    if (!name || !email || !message) {
+      return next(
+        res.status(400).json({
+          success: false,
+          message: "Please provide all details",
+        })
+      );
+    }
+    try {
+      await sendEmail({
+        email: "saagor.16@gmail.com",
+        subject: "GYM WEBSITE CONTACT",
+        message,
+        userEmail: email,
+      });
+      res.status(200).json({
+        success: true,
+        message: "Message Sent Successfully.",
+      });
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        message: " Internal Server Error",
+      });
+    }
+  });
+
+
+app.use(router);
 
 // Set PORT directly in the code
 const PORT = 3000; // Replace 3000 with your desired port
