3293f61d79c809d1009ba13fa89d3b450480b257
saagor16 <saagor.16@gmail.com>
2024-12-01 08:34:21 +0600

    all done

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import express from "express";
+import { config } from "dotenv";
 import cors from "cors";
 import { sendEmail } from "./utils/sendEmail.js";
 
 const app = express();
 const router = express.Router();
 
-// Middleware
-app.use(express.json());
-app.use(cors());
+config({ path: "./config.env" });
 
 app.use(
   cors({
@@ -21,41 +20,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 router.post("/send/mail", async (req, res, next) => {
-    const { name, email, message } = req.body;
-    if (!name || !email || !message) {
-      return next(
-        res.status(400).json({
-          success: false,
-          message: "Please provide all details",
-        })
-      );
-    }
-    try {
-      await sendEmail({
-        email: "saagor.16@gmail.com",
-        subject: "GYM WEBSITE CONTACT",
-        message,
-        userEmail: email,
-      });
-      res.status(200).json({
-        success: true,
-        message: "Message Sent Successfully.",
-      });
-    } catch (error) {
-      res.status(500).json({
+  const { name, email, message } = req.body;
+  if (!name || !email || !message) {
+    return next(
+      res.status(400).json({
         success: false,
-        message: " Internal Server Error",
-      });
-    }
-  });
-
+        message: "Please provide all details",
+      })
+    );
+  }
+  try {
+    await sendEmail({
+      email: "saagor.16@gmail.com",
+      subject: "GYM WEBSITE CONTACT",
+      message,
+      userEmail: email,
+    });
+    res.status(200).json({
+      success: true,
+      message: "Message Sent Successfully.",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: " Internal Server Error",
+    });
+  }
+});
 
 app.use(router);
 
-// Set PORT directly in the code
-const PORT = 3000; // Replace 3000 with your desired port
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT}`);
 });
